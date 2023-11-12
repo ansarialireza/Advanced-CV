a203c7eb5921563e606df28af5671abaf6134c35
alireza <58270983+ansarialireza@users.noreply.github.com>
2023-11-12 18:44:10 +0330

    update blog templates

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -16,7 +16,7 @@ def function():
 def snippet(value,arg=20):
     return value[:20]+". . ."
 
-@register.inclusion_tag('blog/blog-popular-posts.html')
+@register.inclusion_tag('blog/blog-recent-posts.html')
 def popularposts():
     posts=Post.objects.filter(status=1).order_by('-published_date')[:3]
     return {'posts': posts}
